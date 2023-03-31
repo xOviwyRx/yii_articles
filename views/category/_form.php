8a4e964919fface7e90f8bada1d15feab36c38df
Andrey Yurov <a.yurov6@gmail.com>
2023-03-31 13:22:44 +0700

    Fixed styles

--- views/category/_form.php
+++ views/category/_form.php
@@ -14,7 +14,7 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
 
-    <div class="form-group">
+    <div class="form-group mt-3">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
 
